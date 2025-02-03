7665e779a46487554274ea23657ac6e6d328e3e3
Tânia P <taniapestana@gmail.com>
2025-02-03 17:43:24 +0000

    Update workingWithAPI.spec.ts

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,12 +1,24 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({page}) => {
-  await page.goto('https://conduit.bondaracademy.com/')
+  await page.route('https://conduit-api.bondaracademy.com', async route => {
+    const tags = {
+      "tags":[
+        "automation",
+        "playwright"
+      ]
+    }
+    await route.fulfill({
+      body: JSON.stringify(tags)
+    })
+  })
+  await page.goto('https://conduit.bondaracademy.com');
 })
 
 test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+
 });
 
 
