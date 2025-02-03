e328a6ad7050337f0dc30b9482c19677b6f3dccb
Tânia P <taniapestana@gmail.com>
2025-02-03 18:06:23 +0000

    Update workingWithAPI.spec.ts

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test';
 import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({page}) => {
-  await page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
+  //https://conduit-api.bondaracademy.com
+  await page.route('*/**/api/tags', async route => {
     await route.fulfill({
       body: JSON.stringify(tags)
     })
