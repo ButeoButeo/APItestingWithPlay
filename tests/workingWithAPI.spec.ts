dd5a28a742a7f8e3e0d2d42ed031fdf2e2251dec
Tânia P <taniapestana@gmail.com>
2025-02-04 11:17:49 +0000

    Update workingWithAPI.spec.ts

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -23,8 +23,6 @@ test.beforeEach(async ({page}) => {
   await page.goto('https://conduit.bondaracademy.com');
 })
 
-
-
 test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
