810e2470ac868c89ec5f4de1e53cffa4cdc6c179
Tânia P <taniapestana@gmail.com>
2025-02-04 13:01:14 +0000

    Update workingWithAPI.spec.ts

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request } from '@playwright/test';
 import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({page}) => {
@@ -8,27 +8,36 @@ test.beforeEach(async ({page}) => {
       body: JSON.stringify(tags)
     })
   })
-
-  await page.route('*/**/api/articles*', async route => {
-    const response = await route.fetch()
-    const responseBody = await response.json()
-    responseBody.articles[0].title = "This is a test title"
-    responseBody.articles[0].description = "This is a description"
-
-    await route.fulfill({
-      body: JSON.stringify(responseBody)
-    })
-    })
-  
   await page.goto('https://conduit.bondaracademy.com');
 })
 
 test('has title', async ({ page }) => {
+  await page.route('*/**/api/articles*', async route => {
+    const response = await route.fetch()
+    const responseBody = await response.json()
+    responseBody.articles[0].title = "This is a MOCK test title"
+    responseBody.articles[0].description = "This is a MOCK description"
+
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+ 
+  await page.getByText('Global Feed').click()
   // Expect a title "to contain" a substring.
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
-  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
-  await expect(page.locator('app-article-list p').first()).toContainText('This is a description')
-
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK description')
 });
+})
+
+test('delete article', async({page, request})=>{
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+    data:{
+      "user":{"email":"testerqa20252025@gmail.com", "password":"Aa123456789@2025"}
+    }
+  })
+  const responseBody = await response.json()
+  console.log(responseBody)
+})
 
 
